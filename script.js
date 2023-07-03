bc5f59d71d7e1c62b586213c284aebfeb996acbd
Erfan Rezaei <erfan.r8002@gmail.com>
2023-07-03 19:44:08 +0300

    Creating first files and  codes

---
+++ script.js
@@ -0,0 +1,32 @@
+let myLeads = []
+let oldLeads = []
+const inputBtn = document.getElementById("input-btn")
+const inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
+const leadFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if (leadFromLocalStorage)
+{
+    myLeads = leadFromLocalStorage
+    render(myLeads)
+}
+
+function render(leads)
+{
+    let listItems = ""
+    for (let i = 0 ; i < leads.length ; i++)
+  {
+    // the following line runs fine only in a live deployment
+    listItems += `
+    <li>
+       <a target ='_blank' href ='${leads[i]}'>
+              ${leads[i]}
+       </a>
+    </li>
+    `
+  }
+
+     ulEl.innerHTML = listItems
+}
