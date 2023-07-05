892fa9e2ee9e4cdc5c70afcd6c529ab1a8b4245e
Erfan Rezaei <erfan.r8002@gmail.com>
2023-07-05 22:38:28 +0300

    Last changes have been made to the style and scripts.

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ function render(leads)
 
      ulEl.innerHTML = listItems
 }
+
 inputBtn.addEventListener("click", function()
 {
   myLeads.push (inputEl.value)
@@ -43,4 +44,15 @@ deleteBtn.addEventListener("dblclick", function ()
   localStorage.clear()
   myLeads = []
   render(myLeads)
+})
+
+tabBtn.addEventListener("click", function()
+{
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs)
+  {
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify( myLeads))
+    render(myLeads)
+  })
+  
 })
